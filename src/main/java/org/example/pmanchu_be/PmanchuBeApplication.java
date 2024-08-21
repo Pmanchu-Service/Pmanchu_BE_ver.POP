c4829238a9d274eec1d1b15c2f6e2b86eeb52f7a
in-jun <injuninjune@gmail.com>
2024-08-21 17:48:48 +0900

    chore: 프로젝트 초기세팅

---
+++ src/main/java/org/example/pmanchu_be/PmanchuBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.pmanchu_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmanchuBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmanchuBeApplication.class, args);
+    }
+
+}
