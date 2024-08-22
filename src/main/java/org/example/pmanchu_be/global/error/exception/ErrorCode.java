aba146bd174698178ba1b6ce391e2b41824a05ed
Hannah <sunday0206@dsm.hs.kr>
2024-08-22 20:21:14 +0900

    chore: (#4) ErrorCode에 INTERNAL_SERVER_ERROR 추가

--- src/main/java/org/example/pmanchu_be/global/error/exception/ErrorCode.java
+++ src/main/java/org/example/pmanchu_be/global/error/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND(400,"USER-400-1","일치하는 유저를 찾을 수 없습니다.");
+    USER_NOT_FOUND(400,"USER-400-1","일치하는 유저를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(500, "SERVER-500", "서버에서 에러가 발생했습니다.");
 
     private final int httpStatus;
     private final String code;
