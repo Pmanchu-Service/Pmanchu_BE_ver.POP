1e36d4ca595f156c6b0507d0c804b789a042e4ae
ByunDohwi <dohwe0978@gmail.com>
2024-09-16 11:02:11 +0900

    chore: (#6) 기본 엔티티 package 설정

--- src/main/java/org/example/pmanchu_be/domain/user/domain/repository/User.java
+++ src/main/java/org/example/pmanchu_be/domain/user/domain/repository/User.java
@@ -1,2 +1,15 @@
-package org.example.pmanchu_be.domain.user.domain.repository;public class User {
+package org.example.pmanchu_be.domain.user.domain.repository;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class User {
 }
